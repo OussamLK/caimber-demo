ee05fda4254c70c40b0496f106fc3230f58718a8
Oussam <larkem.m.oussam@gmail.com>
2025-05-21 09:52:02 +0100

    fix a null but, still buggy

--- src/app/(questions)/FillInGaps.tsx
+++ src/app/(questions)/FillInGaps.tsx
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import { type FillInGaps } from '../aiQuery';
+import Grading from './Grading';
 
 export default function FillInGapsComp({ q }: { q: FillInGaps }) {
   return (
     <div>
       <p className="font-bold">{q.questionStatement}</p>
+      <Grading grading={q.grading} currentGrade={-2000} />
       <FillInGapsText text={q.textToFill} />
     </div>
   );
@@ -12,12 +15,32 @@ export default function FillInGapsComp({ q }: { q: FillInGaps }) {
 function FillInGapsText({ text }: { text: string }) {
   const slices = splitTextAtGaps(text);
   const inputSandwich = [<span key={0}>{slices[0]}</span>];
+  const [gapValues, setGapValues] = useState<string[]>(
+    Array(slices.length - 1).fill('')
+  );
+  const inputs: React.ReactNode[] = [];
   slices.slice(1).forEach((slice, key) => {
-    inputSandwich.push(
-      <input key={`${key + 1}-input`} className="border-1 m-1 rounded-sm" />
+    const newInput = (
+      <input
+        value={gapValues[key]}
+        onChange={e => {
+          console.debug(e);
+          console.debug(`key is ${key} and gapValue is ${gapValues[key]}`);
+          setGapValues(prev => [
+            ...prev.slice(0, key),
+            e.target.value,
+            ...prev.slice(key + 1),
+          ]);
+        }}
+        key={`${key + 1}-input`}
+        className="border-1 m-1 rounded-sm"
+      />
     );
+    inputSandwich.push(newInput);
+    inputs.push(newInput);
     inputSandwich.push(<span key={`${key + 1}-span`}>{slice}</span>);
   });
+
   return <p>{...inputSandwich}</p>;
 }
 
@@ -26,14 +49,12 @@ type GapLocation = { start: number; end: number }; //end exlusive
 function splitTextAtGaps(text: string): string[] {
   const chuncks: string[] = [];
   let lastGapEnd = 0;
-  console.debug(`gaps are ${getGaps(text)}`);
   getGaps(text).forEach(gapLocation => {
     const chunck = text.slice(lastGapEnd, gapLocation.start);
     chuncks.push(chunck);
     lastGapEnd = gapLocation.end;
   });
   chuncks.push(text.slice(lastGapEnd));
-  console.debug(`returning chuncks ${JSON.stringify(chuncks)}`);
   return chuncks;
 }
 
